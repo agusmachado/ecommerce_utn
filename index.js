f99029951d992ff6f06e5c6c29d74e84fb20e235
agusmachado <agusmachado@gmail.com>
2023-11-13 20:45:22 -0300

    Primera subida

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const rutasAuth = require('./routes/rutasAuth')
+const profileRutas = require('./routes/profile.router')
+
+const passportSetup=require('./config/config')
+const mongoose=require('mongoose')
+const cookieSession =require('cookie-session');
+const passport = require('passport');
+
+require('dotenv').config();
+
+//----------------------------------------- TRAIGO LA DB
+
+const connectDb = require('./db/conexion')
+
+
+const app = express()
+
+
+//---------------------------------------- CONEXIÓN A LA DB
+
+const PORT = process.env.PORT || 3800
+
+connectDb()
+
+//---------------------------------------- ARCHIVOS ESTÁTICOS Y ACTIVACIÓN DE LOS EJS
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+//------------------- Activamos la cookie
+app.use(cookieSession({
+  maxAge:24*60*60*1000,
+  keys:[process.env.COOKIE_KEY]
+}))
+
+
+//------------------- Inicializamos el passport. Ahora sí, comienza a funcionar el passport
+app.use(passport.initialize())
+//-------------------- Activamos la session.
+app.use(passport.session({
+  secret:'secreto',
+  resave:false,
+  saveUninitialized:true
+}))
+
+//------------ RUTAS
+
+app.use('/auth',rutasAuth)
+app.use('/profile',profileRutas)
+
+
+app.get('/', (req, res) => {
+    res.render('home',{user:req.user});
+});
+
+app.use('/', require('./routes/mainRoute'))
+
+app.listen(PORT, () =>{
+    console.log('Se conectó el servidor')
+})
---
+++ routes/mainRoute.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+
+
+const {
+    getProducts,
+    getProductsItem,
+    buscador,
+    filtro
+} = require('../controllers/mainController')
+
+//// ------------------- VISTAS DE LOS MÓDULOS -------------------- ////
+
+
+// ------------------- RUTA PARA MOSTRAR MAIN ------------------- //
+
+
+
+router.get('/', getProducts);
+router.get('/products/:page', getProducts);
+
+
+router.get('', getProducts);
+
+
+// ------------------- RUTA PARA MOSTRAR - "ACERCA DE" ------------------- //
+
+router.get('/main/about', (req, res) => {
+    res.render('about'); 
+});
+
+// ------------------- RUTA PARA MOSTRAR El CARRITO ------------------- //
+
+router.get('/main/carrito', async (req, res) => {
+    res.render('carrito')
+});
+
+// ------------------- RUTA PARA MOSTRAR El FORMULARIO DE COMPRA ------------------- //
+
+router.get('/main/formcCompra', async (req, res) => {
+    res.render('formcCompra')
+});
+
+// ------------------- RUTA PARA MOSTRAR LISTADO DE PRODUCTOS ------------------- //
+
+router.get('/main/listadoProductos', async (req, res) => {
+    res.render('listadoProductos')
+});
+
+// ------------------- RUTA PARA MOSTRAR PRODUCTO ------------------- //
+
+router.get('/main/:productId', getProductsItem);
+
+// ------------------- RUTA PARA MOSTRAR Y BUSCAR PRODUCTOS POR PALABRA CLAVE ------------------- //
+router.get('/buscar-por-keyword', buscador)
+
+
+// ------------------- RUTA PARA MOSTRAR Y BUSCAR PRODUCTOS POR FILTRO ------------------- //
+router.get('/filtro', filtro)
+
+// ------------------- RUTA PARA MOSTRAR LA PAGINA DE USUARIO ------------------- //
+
+router.get('/main/usuario', async (req, res) => {
+    res.render('usuario')
+});
+
+
+module.exports=router
