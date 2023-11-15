31c6ccf0001680f30530154259c5e0f272074195
agusmachado <agusmachado@gmail.com>
2023-11-14 22:10:43 -0300

    index.js

--- index.js
+++ index.js
@@ -3,22 +3,31 @@ const passportSetup = require('./config/config');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const session = require('express-session');  // Agregamos express-session
+
+const app = express();
 
 require('dotenv').config();
+require('express-async-errors');
 
 //----------------------------------------- TRAIGO LA DB
 
 const connectDb = require('./db/conexion');
 
+const rutasAuth = require('./routes/rutasAuth');
+const profileRutas = require('./routes/profile.router');
+const mainRoutes = require('./routes/mainRoute');
 
-const app = express()
+// Creo el middleware que me lee el json para que, después, lo traduzca el express.json()
+app.use(express.urlencoded({ extended: true }));
 
 // Creo el middleware que me transforma los json que me llegan desde los formularios a objeto
 app.use(express.json());
 
 //---------------------------------------- CONEXIÓN A LA DB
 
-const PORT = process.env.PORT || 3800
+const PORT = process.env.PORT || 3800;
+const host = process.env.HOST;
 
 connectDb();
 
@@ -33,28 +42,25 @@ app.use(cookieSession({
   keys: [process.env.COOKIE_KEY]
 }));
 
-//------------------- Inicializamos el passport. Ahora sí, comienza a funcionar el passport
-app.use(passport.initialize())
-//-------------------- Activamos la session.
-app.use(passport.session({
-  secret:'secreto',
-  resave:false,
-  saveUninitialized:true
-}))
+//-------------------- Activamos express-session antes de passport.session
+app.use(session({
+  secret: 'secreto',
+  resave: false,
+  saveUninitialized: true
+}));
+
+// Inicializamos el passport
+app.use(passport.initialize());
+
+// Activamos la session
+app.use(passport.session());
 
 //------------ RUTAS
 
+app.use('/auth', rutasAuth);
+app.use('/profile', profileRutas);
+app.use('/', mainRoutes);
 
-app.use('/auth',rutasAuth)
-app.use('/profile',profileRutas)
-
-
-app.get('/', (req, res) => {
-    res.render('home',{user:req.user});
+app.listen(PORT, host, () => {
+  console.log('Se conectó el servidor');
 });
-
-app.use('/', require('./routes/mainRoute'))
-
-app.listen(PORT, () =>{
-    console.log('Se conectó el servidor')
-})
